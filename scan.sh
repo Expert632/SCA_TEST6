cdfd647b6bd2debef02b40f2ec6d0e155b61031a
Expert632 <machane.salim@gmail.com>
2025-11-17 21:21:49 +0100

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -12,7 +12,7 @@ SCAN_RESULT=$(snyk test --json)
 CRITICAL_COUNT=$(echo "$SCAN_RESULT" | jq '.vulnerabilities[] | select(.severity=="high" or .severity=="critical")' | wc -l)
 
 if [ "$CRITICAL_COUNT" -gt 0 ]; then
-    echo "Vulnérabilité détectée !"
+    echo "Vulnerability detected!"
 else
-    echo "No vulnérabilité détectée."
+    echo "No vulnerability detected."
 fi
